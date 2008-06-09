4c590ed7ca9df4a15cc4343215d1892efd67b32d
arachnid <arachnid@notdot.net>
2008-06-09 19:04:45 +0000

    Branched smtp2web.com

--- admin/request.py
+++ admin/request.py
@@ -10,10 +10,12 @@ application = webapp.WSGIApplication([
     ("/", controllers.IndexPage),
     ("/addmapping", controllers.AddMappingPage),
     ("/test_email", controllers.ReceiveMessagePage),
+    ("/latest_message", controllers.ShowMessagePage),
     ("/api/get_mappings", controllers.GetMappingsPage),
     ("/api/upload_logs", controllers.UploadLogsPage),
     ("/mapping/(_[a-z0-9]+)/delete", controllers.DeleteMappingPage),
     ("/mapping/(_[a-z0-9]+)/logs", controllers.LogsPage),   
+    ("/about", controllers.AboutPage), 
 ])
 
 
